773afbf5320a0af44ae41a2e91e280bca7bd081b
kudaT <timilehinkudaisi01@gmail.com>
2023-02-20 17:26:17 +0100

    bug fixed

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,6 +3,7 @@ import logo from "./img/BENTRACO (13) 1.svg";
 import "./Navbar.css";
 import hamburger from "./img/tabler_menu-2.svg";
 import { Link } from "react-router-dom";
+import cancel from './img/iconoir_cancel.svg'
 
 const Navbar = () => {
   const [expand, setExpand] = useState(true);
@@ -17,21 +18,22 @@ const Navbar = () => {
           <img src={logo} alt="" />
         </Link>
         <div className={expand ? "navs" : "navs navs-NX"}>
-          <Link to="/" className="link">
+          <Link to="/" className="link"  onClick={handler}>
             <h1>Home</h1>
           </Link>
-          <Link to="/services" className="link">
+          <Link to="/services" className="link" onClick={handler}>
             <h1>Services</h1>
           </Link>
-          <Link to="/contact" className="link">
+          <Link to="/contact" className="link" onClick={handler}>
             <h1>Contact Us</h1>
           </Link>
-          <Link to="/about" className="link">
+          <Link to="/about" className="link" onClick={handler}>
             <h1>About Us</h1>
           </Link>
-          <Link to="/booking">
+          <Link to="/booking" onClick={handler}>
             <button className="btnBook">Booking</button>
           </Link>
+        <img src={cancel} alt="" id="cancel" onClick={handler}/>
         </div>
         <div className="hamburger">
           <img src={hamburger} alt="" className="ham" onClick={handler} />
